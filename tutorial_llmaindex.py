2febf445669c4667a7b346f003f5c57463cf44d5
sudesh2022 <aisudesh777@gmail.com>
2023-11-23 13:50:12 +0800

    df

--- tutorial_llmaindex.py
+++ tutorial_llmaindex.py
@@ -1,12 +1,15 @@
 import streamlit as st
-from llama_index import VectorStoreIndex, ServiceContext, Document
-from llama_index.llms import OpenAI
 import openai
-from llama_index import SimpleDirectoryReader
+import time 
 
-import time
+from llama_index.llms import OpenAI
+from llama_index import VectorStoreIndex,SimpleDirectoryReader,Document
 
-st.set_page_config(page_title="App built using watsonx.ai", layout="wide",initial_sidebar_state="auto", page_icon='👧🏻')
+from llama_index import StorageContext,load_index_from_storage,ServiceContext,set_global_handler
+
+
+st.set_page_config(page_title="KYC Application", layout="wide",initial_sidebar_state="auto", page_icon='👧🏻')
+# app sidebar
 # load local CSS styles
 def local_css(file_name):
     with open(file_name) as f:
@@ -23,7 +26,6 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 
-# app sidebar
 with st.sidebar:
     st.markdown("""
                 # What can I ask ? 
@@ -33,7 +35,7 @@ with st.sidebar:
             f"""
             - Which city Anthony did his schooling ?
             - What is Anthonys favorite food ?
-            - What games Vijay and Sudesh can play together ?
+            - Which game Anthony, Vijay, Sudesh can play  ?
             - I want to visit a National Park. Who can advise me on it ?
             - I want to learn swimming. Whom should I speak to ?
           
@@ -45,49 +47,35 @@ with st.container():
     col1,col2 = st.columns([8,3])
 
 
-
 openai.api_key = st.secrets.API_KEY
+st.title("Chat application built with Streamlit docs, powered by LlamaIndex")
 
-
-remove_all_streamlit_icons = """
-  <style>
-        .reportview-container {
-            margin-top: -2em;
-        }
-        #MainMenu {visibility: hidden;}
-        .stDeployButton {display:none;}
-        footer {visibility: hidden;}
-        #stDecoration {display:none;}
-        .body {
-        background-color: #f0f0f0; /* You can replace this with your desired color code */
-        }
-    </style>
-"""
-
-
-#st.markdown(remove_all_streamlit_icons, unsafe_allow_html=True)
-
-
-
-
-st.title("KYC - Know your colleagues powered by IBM watsonx.ai 💬🦙")
-       
 if "messages" not in st.session_state.keys(): # Initialize the chat messages history
     st.session_state.messages = [
-        {"role": "assistant", "content": "This is a fun app. It has information about PS & ITS  Team members! You can ask questions about them"}
+        {"role": "assistant", "content": "Know your Colleagues (KYC), Ask about your colleagues "}
+    
     ]
 
 @st.cache_resource(show_spinner=False)
 def load_data():
-    with st.spinner(text="Loading and indexing the docs – hang tight! This should take 1-2 minutes."):
+    with st.spinner(text="Loading and indexing the Streamlit docs – hang tight! This should take 1-2 minutes."):
         reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
         docs = reader.load_data()
         prompt='''
-        Sudesh, Anthony, Jai, Ashoka, Naren, Manoj, Doyoung, Vijay are people who are part of a team. When someone ask for example who is Vijay, summarize their profile. Answer any other questions based on the data provided. Assume that all questions are based on the information provided. Keep your answers short and  based on facts – do not hallucinate.
+        You are an expert on people. Your job is to answer  questions about people. 
+        Assume that all questions are related to data provided. 
+        Keep your answers short and based on facts – do not hallucinate features.
+        
+        Question: Who is Vijay ?
+        Answer: Vijay has 24 years’ experience in Enterprise Infrastructure Design, Solution Architecture and Cloud Solution & Integration. Vijay has a strong Technical Project Management, Transformation and Transition of customer Data center and Team Management experience. Vijay specialities are Hyperscalers like AWS, Azure, Google and IBM cloud, EMC, IBM & VMware products, Management & Orchestration for Cloud Environment, Virtualization Architecture, Data center Consolidation, Data center Management, Technical presales, BC & DR setup, Backup. Setting up Proof of Concepts to demonstrate the product capability, Team Management & Team Player and evolving every day in the ever-changing world.
+        Question: Who is Sudesh ?
+        Answer: Sudesh has worked in Software for over 22 . Sudesh has a diverse set of skillsets serving in technical roles across consulting, presales, support & services. Sudesh is also a hands-on Architect, and has worked across ASEAN, India  and Australia. 
         '''
-
-        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt=prompt))
+        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", chunk_size=150,chunk_overlap=0,temperature=0.3,max_token=50,system_prompt=prompt))
         index = VectorStoreIndex.from_documents(docs, service_context=service_context)
+        #index.storage_context.persist()
+        #storage_context = StorageContext.from_defaults(persist_dir="./storage")
+        #index = load_index_from_storage(storage_context=storage_context)
         return index
 
 index = load_data()
@@ -102,8 +90,7 @@ for message in st.session_state.messages: # Display the prior chat messages
     with st.chat_message(message["role"]):
         st.write(message["content"])
 
-
-
+# If last message is not from assistant, generate a new response
 # If the last message is not from the assistant, generate a new response
 if st.session_state.messages[-1]["role"] != "assistant":
     with st.chat_message("assistant"):
@@ -113,7 +100,7 @@ if st.session_state.messages[-1]["role"] != "assistant":
             elapsed_time = time.time() - start_time  # Calculate elapsed time
 
             # Check if elapsed time is less than 15 seconds
-            if elapsed_time < 30:
+            if elapsed_time < 20:
                 st.write(response.response)
                 message = {"role": "assistant", "content": response.response}
                 st.session_state.messages.append(message)
@@ -121,4 +108,7 @@ if st.session_state.messages[-1]["role"] != "assistant":
                 st.write("I am sorry, I dont want to keep you waiting. It's not something that I can answer within 15 seconds. Please ask the question in a different manner and I will try to answer within 15 seconds.")
                 message = {"role": "assistant", "content": "Custom message for delayed response"}
 
-            #st.session_state.messages.append(message)  # Add response to message history
+
+
+
+

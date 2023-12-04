af3b1a248e3930e1f0be579d45f59aff5b2334cb
sudesh2022 <aisudesh777@gmail.com>
2023-12-04 14:37:15 +0800

    test

--- upload_bank.py
+++ upload_bank.py
@@ -17,7 +17,8 @@ response = client.chat.completions.create(
         {
           "type": "image_url",
           "image_url": {
-            "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg",
+            #"url": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg",
+            "url":"/Users/sudeshkrishnamoorthy/Documents/CSC2023/search/searchresumes/usualday.jpeg"
           },
         },
       ],
